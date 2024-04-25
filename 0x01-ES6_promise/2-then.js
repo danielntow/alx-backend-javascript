35ef91ce74620a6da6f4380668e770b973bba0f8
danielntow <danielntow@gmail.com>
2024-04-25 05:09:32 +0000

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,6 @@
-// In 2-then.js file
-
 export default function handleResponseFromAPI(promise) {
-    return promise
-      .then((response) => {
-        return { status: 200, body: 'success' };
-      })
-      .catch(() => {
-        return new Error();
-      })
-      .finally(() => {
-        console.log('Finally block executed');
-      });
-  }
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+}
