8a098d08d0df2a7a691b856f6e067bd9a2b03e51
danielntow <danielntow@gmail.com>
2024-04-25 17:00:28 +0000

    es6 classes

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,35 @@
+export default class Currency{
+
+    constructor(code , name ){
+        this._code = code;
+        this._name = name;
+    }
+
+
+    get name(){
+        return this._name;
+    }
+
+    set name(value){
+        if (typeof value !== 'string'){
+            throw new TypeError("Name must be a string");
+        }
+        this._name = value;
+    }
+
+    get code(){
+        return this._code;
+    }
+
+    set code(value){
+        if (typeof value !== 'string'){
+            throw new TypeError("code must be a string");
+        }
+        this._name = value;
+    }
+
+    displayFullCurrency(){
+    return (`${this._name} (${this._code})`)
+    }
+
+}
