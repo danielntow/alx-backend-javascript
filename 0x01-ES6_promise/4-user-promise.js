bb025257a40f9db248687d74b29efdcd4aecb756
danielntow <danielntow@gmail.com>
2024-04-25 05:37:03 +0000

    4-user-promise.js

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,3 +1,8 @@
+// In 4-user-promise.js file
+
 export default function signUpUser(firstName, lastName) {
-    return Promise.resolve({ firstName, lastName });
+    return Promise.resolve({
+      firstName: firstName,
+      lastName: lastName,
+    });
   }
