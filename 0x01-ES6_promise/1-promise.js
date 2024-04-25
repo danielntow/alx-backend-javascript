1fa38891389a49e3b8c63c4f3f2fe6d90fe95bcb
danielntow <danielntow@gmail.com>
2024-04-25 04:49:07 +0000

    return a promisse

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,11 @@
 // In 1-promise.js file
 
 export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: "Success" });
-    } else {
-      reject(new Error("The fake API is not working currently"));
-    }
-  });
+    return new Promise((resolve, reject) => {
+        if (success) {
+            resolve({ status: 200, body: "Success" });
+        } else {
+            reject(new Error("The fake API is not working currently"));
+        }
+    });
 }
