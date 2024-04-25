d0e3c23a2acdb72ba86d789e9aa261e87f1774aa
danielntow <danielntow@gmail.com>
2024-04-25 05:16:24 +0000

    3-all.js

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+// In 3-all.js file
+
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
