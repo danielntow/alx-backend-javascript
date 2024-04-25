b9883428a25db02afdc951a06204a8b8e42c9a78
danielntow <danielntow@gmail.com>
2024-04-25 15:18:03 +0000

    es6 classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,5 @@
 import ClassRoom from "./0-classroom";
 
-// main.js (continued)
 const initializeRooms = () => {
   const sizes = [19, 20, 34];
   const rooms = sizes.map((size) => new ClassRoom(size));
