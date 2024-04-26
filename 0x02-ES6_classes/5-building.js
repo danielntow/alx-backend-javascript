1572d582b370eb73ea8f13a2f2a995e4ad9d382b
danielntow <danielntow@gmail.com>
2024-04-26 04:40:00 +0000

    es6 classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -8,27 +8,27 @@
  *     override the `evacuationWarningMessage` method.
  */
 class Building {
-  constructor(sqft) {
+  constructor (sqft) {
     if (this.constructor !== Building) {
       if (typeof this.evacuationWarningMessage !== 'function') {
         throw new Error(
-            'Class extending Building must override evacuationWarningMessage',
-        );
+          'Class extending Building must override evacuationWarningMessage'
+        )
       }
     }
-    this.sqft = sqft;
+    this.sqft = sqft
   }
 
-  set sqft(sqft) {
+  set sqft (sqft) {
     if (typeof sqft !== 'number') {
-      throw new TypeError('sqft nust be a number');
+      throw new TypeError('sqft nust be a number')
     }
-    this._sqft = sqft;
+    this._sqft = sqft
   }
 
-  get sqft() {
-    return this._sqft;
+  get sqft () {
+    return this._sqft
   }
 }
 
-  export default Building;
+export default Building
