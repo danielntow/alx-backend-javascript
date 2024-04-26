98fe3c6a655f58d57d856aaa8d27da9e40657dcd
danielntow <danielntow@gmail.com>
2024-04-26 05:14:44 +0000

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,13 +1,24 @@
 export default class Building {
   constructor(sqft) {
+    if (typeof sqft !== 'number') throw new Error('sqft must be a number');
     this._sqft = sqft;
-
-    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
-      throw Error('Class extending Building must override evacuationWarningMessage');
-    }
+    if (this.constructor !== Building) this.evacuationWarningMessage();
   }
 
+  // getter for sqft
   get sqft() {
-    return this._sqft;
+    return (this._sqft);
+  }
+
+  // setter for sqft
+  set sqft(thisSqft) {
+    if (typeof thisSqft !== 'number') throw new Error('sqft must be a number');
+    this._sqft = thisSqft;
+  }
+
+  // make subclasses implement this method
+  evacuationWarningMessage() {
+    this.sqft = this._sqft;
+    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
