bcf952504bc0afe2870e6504513207f120ee7392
danielntow <danielntow@gmail.com>
2024-04-26 05:00:50 +0000

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,5 +1,4 @@
 export default class Building {
-<<<<<<< HEAD
   constructor(sqft) {
     this._sqft = sqft;
 
@@ -12,17 +11,3 @@ export default class Building {
     return this._sqft;
   }
 }
-=======
-    constructor(sqft) {
-      this._sqft = sqft;
-
-      if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
-        throw Error('Class extending Building must override evacuationWarningMessage');
-      }
-    }
-
-    get sqft() {
-      return this._sqft;
-    }
-  }
->>>>>>> 247c0be ( es6 classes)
