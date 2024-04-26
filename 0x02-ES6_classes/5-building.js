f3864d41e8bc3a8cd694fbb27dc2e76dbf6b2792
danielntow <danielntow@gmail.com>
2024-04-26 04:47:30 +0000

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,32 +1,13 @@
-/**
- * Represents a building with a given square footage.
- *
- * @class Building
- * @param {number} sqft - The square footage of the building.
- * @throws {TypeError} Throws an error if `sqft` is not a number.
- * @throws {Error} Throws an error if a class extending `Building` does not
- *     override the `evacuationWarningMessage` method.
- */
 export default class Building {
-  constructor (sqft) {
-    if (this.constructor === Building) {
-      throw new Error('Building is an abstract class and cannot be instantiated directly.')
+  constructor(sqft) {
+    this._sqft = sqft;
+
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
-    this._sqft = sqft
   }
 
-  get sqft () {
-    return this._sqft
-  }
-
-  set sqft (value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('sqft must be a Number')
-    }
-    this._sqft = value
-  }
-
-  evacuationWarningMessage () {
-    throw new Error('Class extending Building must override evacuationWarningMessage')
+  get sqft() {
+    return this._sqft;
   }
 }
