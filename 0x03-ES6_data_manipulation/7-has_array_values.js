5eeefb4c61f9eba9caa1796afc294f1aa950425e
danielntow <danielntow@gmail.com>
2024-04-30 05:30:19 +0000

    data manipulation

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,6 @@
+const hasValuesFromArray = (set, array) => {
+  // Check if every element in the array exists in the set
+  return array.every((element) => set.has(element));
+};
+
+export default hasValuesFromArray;
