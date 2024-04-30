6a717208cc804f9ab837cd2a379283d9e60a2ace
danielntow <danielntow@gmail.com>
2024-04-30 04:29:56 +0000

    data manipulation

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,15 @@
-function getListStudents () {
+/**
+ * Retrieves a list of student objects.
+ *
+ * @returns {Object[]} An array of student objects, each with an id,
+ * firstName, and location property.
+ */
+function getListStudents() {
   return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
-  ]
+    { id: 1, firstName: "Guillaume", location: "San Francisco" },
+    { id: 2, firstName: "James", location: "Columbia" },
+    { id: 5, firstName: "Serena", location: "San Francisco" },
+  ];
 }
 
-export default getListStudents
+export default getListStudents;
