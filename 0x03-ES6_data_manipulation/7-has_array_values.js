7728646bdcc7fb67a3ff00fa9ccbc0b6d2b54b8b
danielntow <danielntow@gmail.com>
2024-04-30 05:33:07 +0000

    Update 7-has_array_values.js

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,6 +1,3 @@
-function hasValuesFromArray(set, array) {
-  // Check if every element in the array exists in the set
-  return array.every((element) => set.has(element));
-};
-
-export default hasValuesFromArray;
+export default function hasValuesFromArray(set, array) {
+  return array.every((value) => set.has(value));
+}
