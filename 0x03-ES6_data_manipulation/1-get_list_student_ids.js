2bbba4f7202065172c427664b1f34f2aae9e8206
danielntow <danielntow@gmail.com>
2024-04-30 04:40:04 +0000

    data manipulation

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+const getListStudentIds = (studentList) => {
+    if (!Array.isArray(studentList)) {
+        return [];
+    }
+
+    return studentList.map(student => student.id);
+};
+
+export default getListStudentIds;
