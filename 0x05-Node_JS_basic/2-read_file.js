6f626ed470815c915ab27d685b92b2b495247d4c
danielntow <danielntow@gmail.com>
2024-06-19 05:31:33 +0000

    0x05-Node_JS_basic

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,37 +1,24 @@
-// 2-read_file.js
 const fs = require('fs');
 
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    const lines = data.trim().split('\n');
-
-    if (lines.length === 0) {
-      throw new Error('Cannot load the database');
-    }
-
-    const studentData = {};
-    let totalStudents = 0;
-
-    for (const line of lines) {
-      if (line.trim()) {
-        const [firstname, field] = line.split(',');
-        if (firstname && field) {
-          if (!studentData[field]) {
-            studentData[field] = [];
-          }
-          studentData[field].push(firstname);
-          totalStudents++;
-        }
-      }
-    }
-
-    console.log(`Number of students: ${totalStudents}`);
-    for (const [field, students] of Object.entries(studentData)) {
-      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
-    }
-  } catch (error) {
-    throw new Error('Cannot load the database');
+    const result = [];
+    data.split('\n').forEach((data) => {
+      result.push(data.split(','));
+    });
+    result.shift();
+    const newis = [];
+    result.forEach((data) => newis.push([data[0], data[3]]));
+    const fields = new Set();
+    newis.forEach((item) => fields.add(item[1]));
+    const final = {};
+    fields.forEach((data) => { (final[data] = 0); });
+    newis.forEach((data) => { (final[data[1]] += 1); });
+    console.log(`Number of students: ${result.filter((check) => check.length > 3).length}`);
+    Object.keys(final).forEach((data) => console.log(`Number of students in ${data}: ${final[data]}. List: ${newis.filter((n) => n[1] === data).map((n) => n[0]).join(', ')}`));
+  } catch (E) {
+    throw Error('Cannot load the database');
   }
 }
 
