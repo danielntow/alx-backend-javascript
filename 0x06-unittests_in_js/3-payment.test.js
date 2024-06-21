595b8ac8477a0dab3ccfb270c5f32182c8ccd589
danielntow <danielntow@gmail.com>
2024-06-21 05:53:38 +0000

    Create 3-payment.test.js

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,13 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber with correct arguments', () => {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    spy.restore();
+  });
+});
