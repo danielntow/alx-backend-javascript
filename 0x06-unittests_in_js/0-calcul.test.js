0651af44e65f9367817ccbea1eeb2e68319bf4aa
danielntow <danielntow@gmail.com>
2024-06-21 05:39:42 +0000

    Update 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,21 +1,31 @@
-const assert = require('assert');
-const {it, describe} = require('mocha');
 const calculateNumber = require('./0-calcul');
+const assert = require('assert');
 
-describe('calculateNumber', () => {
-    it('checking if numbers round', () => {
-        assert.equal(calculateNumber(1, 3), 4);
-    });
-    it('checking if numbers round 2nd round', () => {
-        assert.equal(calculateNumber(1, 3.7), 5);
-    });
-    it('checking if numbers round 3nd round', () => {
-        assert.equal(calculateNumber(1.2, 3.7), 5);
-    });
-    it('checking if numbers round 4nd round', () => {
-        assert.equal(calculateNumber(1.5, 3.7), 6);
-    });
-    it('checking negative return', () => {
-        assert.equal(calculateNumber(-1.3, -3.7), -5);
-      });
-});
+describe("Test case", function() {
+  it("test if 1.2 and 5 is equal to 5", function() {
+    assert.strictEqual(calculateNumber(1.2, 4), 5);
+  });
+  it("test if 1.8 and 4.6 is equal to 6", function() {
+    assert.strictEqual(calculateNumber(1.8, 4.6), 7);
+  });
+
+  it("test if 2 and 3 is equal to 5", function() {
+    assert.strictEqual(calculateNumber(2, 3), 5);
+  });
+
+  it("test if 0 and 0 is equal to 0", function() {
+    assert.strictEqual(calculateNumber(0, 0), 0);
+  });
+
+  it("test if negatif number works", function() {
+    assert.strictEqual(calculateNumber(-2, -7.8), -10);
+  });
+
+  it("test if negatif and positif number wors", function() {
+    assert.strictEqual(calculateNumber(2.8, -8), -5);
+  });
+
+  it("test if negatif and positif number wors", function() {
+    assert.strictEqual(calculateNumber(8, -2.8), 5);
+  });
+})
