0fea215377171a0f370f5e43d9fc61fccbea1567
danielntow <danielntow@gmail.com>
2024-06-26 13:33:51 +0000

    unitest

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -7,7 +7,6 @@ const chai = require('chai');
 describe('basic integration test', () => {
   const API_URL = 'http://localhost:7865';
   
-
   it('check correct response message', () => new Promise((done) => {
     request.get(`${API_URL}/`, (_err, res, body) => {
       chai.expect(res.statusCode).to.be.equal(200);
